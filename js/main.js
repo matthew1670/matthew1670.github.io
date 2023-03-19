51d9dc8ef59bb69836defaf484bb93e4197f3218
Matthew Wood <matthew1670@users.noreply.github.com>
2023-03-19 22:21:19 +0000

    Update carousel styling

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
-const swiper = new Swiper('.swiper', {
+/** @format */
+
+const swiper = new Swiper(".swiper", {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
-    slidesPerGroup:3,
-    spaceBetween: 10,
+    slidesPerView: 2,
+    spaceBetween: 20,
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: ".swiper-scrollbar",
+        enabled: false,
     },
-  });
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        },
+    },
+});
